f88a2b2d6ee0ed6c6ffcf051272428e75de65557
BahadurAliShah <96332131+BahadurAliShah@users.noreply.github.com>
2022-09-18 16:57:41 +0000

    Deploying to gh-pages from @ BahadurAliShah/CleanCityDubai@df91f8f897df70dc91506c20dc13def8aca1f53c 🚀

---
+++ constants.js
@@ -0,0 +1,29 @@
+import HouseCleaning from "./serviceImages/houseCleaning.jpg"
+
+export const services = [
+    {
+        title: "House Cleaning",
+        desc: "Unlike other cleaning companies, we are strictly focused on both cleaning and organizing your home. Our approach is to create a happy home for you by making it fresh and hygiene. This is the reason why we are the leading house cleaning services provider in Dubai.",
+        photo: HouseCleaning
+    },
+    {
+        title: "House Cleaning",
+        desc: "Unlike other cleaning companies, we are strictly focused on both cleaning and organizing your home. Our approach is to create a happy home for you by making it fresh and hygiene. This is the reason why we are the leading house cleaning services provider in Dubai.",
+        photo: HouseCleaning
+    },
+    {
+        title: "House Cleaning",
+        desc: "Unlike other cleaning companies, we are strictly focused on both cleaning and organizing your home. Our approach is to create a happy home for you by making it fresh and hygiene. This is the reason why we are the leading house cleaning services provider in Dubai.",
+        photo: HouseCleaning
+    },
+    {
+        title: "House Cleaning",
+        desc: "Unlike other cleaning companies, we are strictly focused on both cleaning and organizing your home. Our approach is to create a happy home for you by making it fresh and hygiene. This is the reason why we are the leading house cleaning services provider in Dubai.",
+        photo: HouseCleaning
+    },
+    {
+        title: "House Cleaning",
+        desc: "Unlike other cleaning companies, we are strictly focused on both cleaning and organizing your home. Our approach is to create a happy home for you by making it fresh and hygiene. This is the reason why we are the leading house cleaning services provider in Dubai.",
+        photo: HouseCleaning
+    }
+]
