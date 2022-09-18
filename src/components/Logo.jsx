f5a87f58b33725bcaff056e22a0839093b7597dd
BahadurAliShah <96332131+BahadurAliShah@users.noreply.github.com>
2022-09-18 15:35:26 +0000

    Deploying to gh-pages from @ BahadurAliShah/CleanCityDubai@6118c40b316f6e76af1734d542e4af1fdc4f072c 🚀

--- src/components/Logo.jsx
+++
@@ -1,8 +0,0 @@
-import Image from "next/future/image";
-import CCBCSLogo from "@/images/logos/logo.svg"
-
-export function Logo(props) {
-  return (
-    <Image src={CCBCSLogo} alt="Logo" width="40" height="40"{...props}/>
-  )
-}
